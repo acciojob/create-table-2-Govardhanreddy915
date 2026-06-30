859dbe3a1a18393437c4aa676cbedd6f71752042
Madapa Govardhan reddy <mgovardhanreddy915@gmail.com>
2026-06-30 12:15:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 function createTable() {
-    //Write your code here
-  
+    let rn = prompt("Input number of rows");
+    let cn = prompt("Input number of columns");
+
+    if (isNaN(rn) || isNaN(cn)) {
+        return;
+    }
+
+    rn = Number(rn);
+    cn = Number(cn);
+
+    if (rn <= 0 || cn <= 0) {
+        alert("Please enter positive numbers.");
+        return;
+    }
+
+    let table = document.getElementById("myTable");
+
+    table.innerHTML = "";
+
+    for (let i = 0; i < rn; i++) {
+        let row = table.insertRow();
+
+        for (let j = 0; j < cn; j++) {
+            let cell = row.insertCell();
+            cell.innerHTML = `Row-${i} Column-${j}`;
+        }
+    }
 }
